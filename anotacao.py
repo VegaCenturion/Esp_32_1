b8708898268608c2570708222d8cba3f14c75537
Ala-R-F <89356326+Ala-R-F@users.noreply.github.com>
2022-08-25 10:47:41 -0300

    Create anotacao.py

---
+++ anotacao.py
@@ -0,0 +1,13 @@
+/*
+
+Anotações de usos possíveis para seu programa:
+
+*/
+
+// Move arquivo de uma pasta para outra
+import shutil
+
+original = r'original path where the file is currently stored\file name.file extension'
+target = r'target path where the file will be moved\file name.file extension'
+
+shutil.move(original, target)
